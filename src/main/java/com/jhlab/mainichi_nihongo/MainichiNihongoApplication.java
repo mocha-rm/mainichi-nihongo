94462d5efe1ab96fec4a36ac560ce5c759f5b4e2
Park JaeHong <blogerhong@naver.com>
2025-05-02 23:20:30 +0900

    add : git init

---
+++ src/main/java/com/jhlab/mainichi_nihongo/MainichiNihongoApplication.java
@@ -0,0 +1,13 @@
+package com.jhlab.mainichi_nihongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainichiNihongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainichiNihongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhlab/mainichi_nihongo/MainichiNihongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhlab.mainichi_nihongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainichiNihongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
