1b10e16aaed7ddd8526e0b580b690861c60146ad
Park JaeHong <blogerhong@naver.com>
2025-05-20 01:25:07 +0900

    feat : 이메일 발송 기능 추가
    - 정기적으로 이메일 발송을 위한 스케쥴러 추가
    - 임시 테스트용 콘텐츠 작성

--- src/main/java/com/jhlab/mainichi_nihongo/domain/subscribe/service/SubscribeService.java
+++ src/main/java/com/jhlab/mainichi_nihongo/domain/subscribe/service/SubscribeService.java
@@ -5,13 +5,17 @@ import com.jhlab.mainichi_nihongo.domain.subscribe.repository.SubscriberReposito
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SubscribeService {
     private final SubscriberRepository subscriberRepository;
 
+    @Transactional
     public void subscribe(String email) {
         if (!isValidEmail(email)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "유효하지 않은 이메일입니다.");
@@ -24,11 +28,17 @@ public class SubscribeService {
         subscriberRepository.save(new Subscriber(email));
     }
 
+    @Transactional
     public void unsubscribe(String email) {
         Subscriber subscriber = subscriberRepository.findByEmail(email)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "등록되지 않은 이메일입니다."));
 
         subscriberRepository.delete(subscriber);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Subscriber> getAllSubscribers() {
+        return subscriberRepository.findAll();
     }
 
     private boolean isValidEmail(String email) {
